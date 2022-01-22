3453074c264cdceeb7a59c054739636392481872
Vedant Prajapati <40185967+vedantprajapati@users.noreply.github.com>
2022-01-22 13:18:12 -0500

    working on random roles

--- roles.py
+++ roles.py
@@ -1,9 +1,9 @@
 import random
 
-ROLE_INFO = {"Werewolf": {"About": "see the other werewolves", "Required": True},
-             "Camper": {"About": "we vibing fham", "Limit": 1000, "Required": True},
-             "BFF-1": {"About": "half of best friends", "Limit": 1, "Required": False},
-             "BFF-2": {"About": "other half of best friends", "Limit": 1, "Required": False},
+ROLE_INFO = {"Werewolf": {"About": "see the other werewolves","Limit": 1, "Required": True},
+             "Camper": {"About": "we vibing fham", "Limit": 7, "Required": True},
+             "bff_1": {"About": "half of best friends", "Limit": 1, "Required": False},
+             "bff_2": {"About": "other half of best friends", "Limit": 1, "Required": False},
              "Camp Counselor": {"About":"does counselling", "Limit": 1, "Required": False},
              "Wannabe": {"About": "does something", "Limit": 1, "Required": False},
              "Introvert": {"About": "we vibing fham 2.0", "Limit": 1, "Required": False}   
@@ -34,17 +34,57 @@ class Player:
         self.last_voted = name
         return self.round, self.last_voted
     
+# camp counsellor can look at 1 persons role at night
+# have time state in gamestate
+# get 2/3 unpicked roles from group
+# 1 werewolf unless > 6
+# 1 camp counsellor only if > 5 ppl
+
+    
 
 class GameState:
-    def __init__(self, player_names):
+    def __init__(self, player_names, extra_roles=False):
         self.num_players = len(player_names)
-        random_roles = self.set_random_roles()
+        random_roles = self.set_random_roles(extra_roles)
         self.players = [Player(player_names[person], random_roles[person]) for person in range(self.num_players)]
         
     
-    def set_random_roles(self):
-        rand_roles = ROLE_INFO.keys()
-        random.shuffle(rand_roles)
+    def set_random_roles(self, extra_roles):
+        roles = {}
+        
+        if not extra_roles:
+            for role in ROLE_INFO:
+                if role['Required']:
+                    roles[role]= ROLE_INFO[role]
+        else: 
+            roles = ROLE_INFO.copy()
+        
+        if self.num_players>5 and extra_roles:
+            players_left = self.num_players
+            role_limits = {"werewolf":2, "camp_councellor":1, "wannabe":1, "introvert":1, "bff_1":0, "bff_2":0, "camper":0,}
+            players_left -= 4
+            if players_left >= 2:
+                role_limits['bff_1'] = 1
+                role_limits['bff_2'] = 1
+                players_left -= (role_limits['bff_1'] + role_limits['bff_2'])
+            else:
+                role_limits['camper'] = 1
+                players_left -= role_limits['camper']          
+            if players_left != 0:
+                role_limits['camper'] += players_left
+                players_left -= role_limits['camper']
+
+            players = []
+            for role in role_limits:
+                limit = role_limits[role]
+                while limit != 0:
+                    players.append[role]
+            random.shuffle(players)
+            return players
+        
+        if self.num_players>5:
+            pass
+            
         return rand_roles
     
     def tally_votes(self):
