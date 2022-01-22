2e5c00b36c76bd437106c4b66b7792bfd1c98722
Lance Canlas <lancecanlas25@gmail.com>
2022-01-22 00:38:48 -0500

    Template for bot

---
+++ bot.py
@@ -0,0 +1,50 @@
+# bot.py
+import os
+import random
+import asyncio
+
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+@bot.command(name='timer', help='Responds with a random quote from Brooklyn 99')
+async def timer(ctx):#, seconds):
+    try:
+        secondint = int(15)
+        if secondint > 300:
+            await ctx.send("I dont think im allowed to do go above 300 seconds.")
+            raise BaseException
+        if secondint <= 0:
+            await ctx.send("I dont think im allowed to do negatives")
+            raise BaseException
+        message = await ctx.send("Timer: {seconds}")
+        while True:
+            secondint -= 1
+            if secondint == 0:
+                await message.edit(content="Ended!")
+                break
+            await message.edit(content=f"Timer: {secondint}")
+            await asyncio.sleep(1)
+        await ctx.send(f"{ctx.author.mention} Your countdown Has ended!")
+    except ValueError:
+        await ctx.send("Must be a number!")
+
+bot.run(TOKEN)
