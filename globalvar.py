6d14c4e3f8b2262645472a3fdd3e828265097683
Hannah <hcruiz0211@gmail.com>
2022-01-23 17:54:39 -0500

    Actually adding globalvar.py file lol oops

---
+++ globalvar.py
@@ -0,0 +1,15 @@
+####### GLOBAL VARIABLES #########
+unicode_letters = ["🇦", "🇧", "🇨", "🇩", "🇪", "🇫", "🇬", "🇭", "🇮"]
+channel_list=[]
+userlist=[]
+poll_list=[]
+player_list=[]
+werewolf_list = []
+camper_list = []
+wannabe_list = []
+introvert_list = []
+best_friend_list = []
+camp_counselor_list = []
+
+NUM_OF_EACH_ROLE = {"Werewolf":0, "Camp Counselor":0, "Wannabe":0, "Introvert":0, "bffpair":0, "Camper":0}
+CUSTOM_ROLES = False
